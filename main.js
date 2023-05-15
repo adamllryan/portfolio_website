5232f6594e77ffcba116a0f577e76a3a6b93ac61
Adam <adamllryan@gmail.com>
2023-05-15 18:51:11 -0400

    data read?

--- main.js
+++ main.js
@@ -1,19 +1,44 @@
-//const { faker } = require('@faker-js/faker');
-//console.log(faker.lorem.paragraph(1));
 const maxEntries = 10;
-function createRow() {
+const defaultImage = "https://github.com/adamllryan.png";
+const pathHeader = "https://github.com/adamllryan/";
+const jsonPath = "display-projects.json";
+import data from './display-projects.json';
+function populateRow(src, data) {
+    //image
+    let img = document.createElement("img");
+    let url = data.imgsrc;
+    if (url!="") {
+        img.src = url;
+    } else {
+        img.src = defaultImage;
+    }
+    src.appendChild(img);
+    //title
 
-    return "<tr>Row</tr>"
+    //description
+    return "";
 }
-
+function generateImage(url) {
+    
+    
+    return img;
+}
 function populateTable() {
-    let count = 0;
+    let rowCount = 0;
     let isFinished = false;
     let table = document.getElementById("projects");
-    while (!isFinished && count < maxEntries) {
-        let row = table.insertRow(count);
-        row.innerHTML = "Project " + count;
-        count++;
+    let projects = JSON.parse(data);// = JSON.parse(jsonData);
+    // fetch('./display-projects.json')
+    // //fetch('https://adamllryan.github.io/data/display-projects.json')
+    // .then(res => res.json())
+    // .then(json => {
+    //     projects = JSON.parse(json);
+    // });
+    
+    while (rowCount < maxEntries && rowCount < projects.count) {
+        let row = table.insertRow(rowCount);
+        row.innerHTML = populateRow(row, projects[rowCount]);
+        rowCount++;
     }
 }
 
