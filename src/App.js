d452f5f0168eb0048cf360c84cf3fb74cd1b0618
Adam <adamllryan@gmail.com>
2024-10-20 17:44:12 -0400

    build error

--- src/App.js
+++ src/App.js
@@ -9,125 +9,126 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
 function App() {
   let [projects, setProjects] = useState([]);
-  const getVH = () => {
-    return Math.max(
-      document.documentElement.clientHeight || 0,
-      window.innerHeight || 0,
-    );
-  };
-  const getIntroBottomHeight = () => {
-    let vh = getVH();
-    let bioBottom = vh / 2 + document.querySelector(".bio").offsetHeight / 2;
-    let headerBottom =
-      vh / 2 + document.querySelector(".header").offsetHeight / 2;
-    return Math.max(bioBottom, headerBottom);
-  };
-
-  const updateArrowVisual = () => {
-    let scrollArrow = document.querySelector(".scroll-arrow");
-    let maxHeight = getIntroBottomHeight();
-    let vh = getVH();
-    if (maxHeight >= 0.95 * vh) {
-      // if bottom of bio or header is greater than 5% of the viewport height, we need to disable the arrow
-      console.log("Removing arrow: too small" + maxHeight + " " + vh);
-      scrollArrow.style.bottom = "5vh";
-      scrollArrow.style.display = "none";
-    } else if (maxHeight > 0.75 * vh) {
-      // if bottom of bio or header is greater than 20% of the viewport height, we need to force 5vh
-      console.log("Forcing arrow to 5vh: too small " + maxHeight + " " + vh);
-      scrollArrow.style.display = "block";
-      scrollArrow.style.bottom = "5vh";
-    } else if (
-      document.body.scrollTop < 10 &&
-      document.documentElement.scrollTop < 50
-    ) {
-      // When big enough and at the top of the page, we need to set the arrow to 20vh so the arrow is more visible
-      console.log("Setting arrow to 20vh: at top");
-      scrollArrow.style.display = "block";
-      scrollArrow.style.bottom = "20vh";
-    } else if (
-      document.body.scrollTop < vh &&
-      document.documentElement.scrollTop < vh
-    ) {
-      // When big enough and the user starts scrolling, set the arrow to 5vh for fun, and begin to fade opacity
-      console.log("Setting arrow to 5vh and fading: scrolling");
-      scrollArrow.style.display = "block";
-      scrollArrow.style.bottom = "5vh";
-    }
-    scrollArrow.style.opacity = 1 - document.documentElement.scrollTop / 200;
-  };
-
-  const updateWaveVisual = () => {
-    let wave = document.querySelector(".bg-wave-1");
-    // Simpler than arrow, just move it to 105vh if user scrolls
-    if (document.documentElement.scrollTop > 10) {
-      wave.style.bottom = "-14vh";
-    } else {
-      wave.style.bottom = "0vh";
-    }
-  };
-
-  const updateIntroVisual = () => {
-    // shrinks as we scroll down
-    let intro = document.querySelector(".intro");
-    intro.style.scale = Math.min(
-      1 - document.documentElement.scrollTop / (2 * getVH()),
-      1,
-    );
-  };
-
-  const updateTimelineScroll = () => {
-    let timeline = document.querySelector(".timeline");
-    if (document.documentElement.scrollTop < 10) {
-      timeline.scrollTop = 0;
-    }
-  };
-
-  const updateListeners = () => {
-    // only add listener if not mobile
-    if (window.innerWidth > 768) {
-      console.log("Adding listeners");
-      window.onresize = function () {
-        updateArrowVisual();
-        updateWaveVisual();
-      };
-
-      window.onscroll = function () {
-        updateArrowVisual();
-        updateWaveVisual();
-        updateTimelineScroll();
-        updateIntroVisual();
-      };
-    } else {
-      //remove waves
-      let wave = document.querySelector(".bg-wave-1");
-      if (wave) wave.style.display = "none";
-      // remove header img
-      let header = document.querySelector(".header__img");
-      if (header) header.style.display = "none";
-      // set to flex column
-      let intro = document.querySelector(".intro");
-      if (intro) intro.style.flexDirection = "column";
-      // set arrow bottom to 5vh
-      let arrow = document.querySelector(".scroll-arrow");
-      if (arrow) arrow.style.bottom = "0vh";
-      // set project cards min width to 90vw
-      let projects = document.querySelectorAll(".project__card");
-
-      projects.forEach((project) => {
-        project.style.minWidth = "90vw";
-        project.style.maxWidth = "90vw";
-      });
-
-      // still reset scroll to top
-      window.onscroll = function () {
-        updateTimelineScroll();
-        updateIntroVisual();
-      };
-    }
-  };
 
   useEffect(() => {
+    const getVH = () => {
+      return Math.max(
+        document.documentElement.clientHeight || 0,
+        window.innerHeight || 0,
+      );
+    };
+    const getIntroBottomHeight = () => {
+      let vh = getVH();
+      let bioBottom = vh / 2 + document.querySelector(".bio").offsetHeight / 2;
+      let headerBottom =
+        vh / 2 + document.querySelector(".header").offsetHeight / 2;
+      return Math.max(bioBottom, headerBottom);
+    };
+
+    const updateArrowVisual = () => {
+      let scrollArrow = document.querySelector(".scroll-arrow");
+      let maxHeight = getIntroBottomHeight();
+      let vh = getVH();
+      if (maxHeight >= 0.95 * vh) {
+        // if bottom of bio or header is greater than 5% of the viewport height, we need to disable the arrow
+        console.log("Removing arrow: too small" + maxHeight + " " + vh);
+        scrollArrow.style.bottom = "5vh";
+        scrollArrow.style.display = "none";
+      } else if (maxHeight > 0.75 * vh) {
+        // if bottom of bio or header is greater than 20% of the viewport height, we need to force 5vh
+        console.log("Forcing arrow to 5vh: too small " + maxHeight + " " + vh);
+        scrollArrow.style.display = "block";
+        scrollArrow.style.bottom = "5vh";
+      } else if (
+        document.body.scrollTop < 10 &&
+        document.documentElement.scrollTop < 50
+      ) {
+        // When big enough and at the top of the page, we need to set the arrow to 20vh so the arrow is more visible
+        console.log("Setting arrow to 20vh: at top");
+        scrollArrow.style.display = "block";
+        scrollArrow.style.bottom = "20vh";
+      } else if (
+        document.body.scrollTop < vh &&
+        document.documentElement.scrollTop < vh
+      ) {
+        // When big enough and the user starts scrolling, set the arrow to 5vh for fun, and begin to fade opacity
+        console.log("Setting arrow to 5vh and fading: scrolling");
+        scrollArrow.style.display = "block";
+        scrollArrow.style.bottom = "5vh";
+      }
+      scrollArrow.style.opacity = 1 - document.documentElement.scrollTop / 200;
+    };
+
+    const updateWaveVisual = () => {
+      let wave = document.querySelector(".bg-wave-1");
+      // Simpler than arrow, just move it to 105vh if user scrolls
+      if (document.documentElement.scrollTop > 10) {
+        wave.style.bottom = "-14vh";
+      } else {
+        wave.style.bottom = "0vh";
+      }
+    };
+
+    const updateIntroVisual = () => {
+      // shrinks as we scroll down
+      let intro = document.querySelector(".intro");
+      intro.style.scale = Math.min(
+        1 - document.documentElement.scrollTop / (2 * getVH()),
+        1,
+      );
+    };
+
+    const updateTimelineScroll = () => {
+      let timeline = document.querySelector(".timeline");
+      if (document.documentElement.scrollTop < 10) {
+        timeline.scrollTop = 0;
+      }
+    };
+
+    const updateListeners = () => {
+      // only add listener if not mobile
+      if (window.innerWidth > 768) {
+        console.log("Adding listeners");
+        window.onresize = function () {
+          updateArrowVisual();
+          updateWaveVisual();
+        };
+
+        window.onscroll = function () {
+          updateArrowVisual();
+          updateWaveVisual();
+          updateTimelineScroll();
+          updateIntroVisual();
+        };
+      } else {
+        //remove waves
+        let wave = document.querySelector(".bg-wave-1");
+        if (wave) wave.style.display = "none";
+        // remove header img
+        let header = document.querySelector(".header__img");
+        if (header) header.style.display = "none";
+        // set to flex column
+        let intro = document.querySelector(".intro");
+        if (intro) intro.style.flexDirection = "column";
+        // set arrow bottom to 5vh
+        let arrow = document.querySelector(".scroll-arrow");
+        if (arrow) arrow.style.bottom = "0vh";
+        // set project cards min width to 90vw
+        let projects = document.querySelectorAll(".project__card");
+
+        projects.forEach((project) => {
+          project.style.minWidth = "90vw";
+          project.style.maxWidth = "90vw";
+        });
+
+        // still reset scroll to top
+        window.onscroll = function () {
+          updateTimelineScroll();
+          updateIntroVisual();
+        };
+      }
+    };
+
     const fetchProjects = async () => {
       try {
         let res = await fetch(
@@ -143,7 +144,6 @@ function App() {
       }
     };
     fetchProjects();
-
     updateListeners();
   }, []);
   return (
