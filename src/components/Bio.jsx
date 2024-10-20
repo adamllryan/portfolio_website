5961651a455d939fe046e6a0e82fdf8791c06bee
Adam <adamllryan@gmail.com>
2024-10-20 12:58:31 -0400

    update bio

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -4,7 +4,15 @@ const Bio = () => {
   return (
     <div class="bio">
         <div class="bio__content">
-            Passionate software engineer and technology hobbyist for nearly a decade. I have always enjoyed involving myself in engineering and automation projects and have expanded into the web development, artificial intelligence, and networking domains.
+            <p>
+                    As a software engineer, I bring a comprehensive background in computer science, specializing in <b>machine learning</b>, <b>full-stack</b> development, and automation. I also enjoy working in networking and <b>embedded</b> systems. 
+            </p>
+            <p>
+                    Currently, I am a full-time student at the Ohio State University, pursuing a <b>Master's</b> degree in Computer Science & Engineering. I am involved in <b>research</b> in artificial intelligence in affiliation with Ohio State University and University of Illinois Urbana-Champagin. I am graduating in May, 2025.
+            </p>
+            <p>
+                    Upon graduation, I am returning to <b>Fidelity Investments</b> as an associate software engineer in Durham, North Carolina. 
+            </p>
         </div>
     </div>
   )
